022d2b36db095912f668e6c8b6b74519a61a8b9b
Florian Giesemann <fgiesemann@proton.me>
2025-10-18 11:55:28 +0200

    Score mate positions

--- include/chessengine/evaluation.h
+++ include/chessengine/evaluation.h
@@ -41,12 +41,21 @@ private:
 
 class Evaluator {
 public:
+    Evaluator() = default;
     explicit Evaluator(EvaluatorConfig config) : m_config{std::move(config)} {}
     auto evaluate(const chesscore::Position &position, chesscore::Color color) const -> Score;
 
+    /**
+     * \brief Checks if the position is mate.
+     *
+     * Checks, if the player who moves next is in checkmate.
+     * \param position The position.
+     * \return If the active polayer is checkmate or not.
+     */
+    static auto is_mate(const chesscore::Position &position) -> bool;
     auto countup_material(const chesscore::Position &position, chesscore::Color color) const -> Score;
 private:
-    EvaluatorConfig m_config;
+    EvaluatorConfig m_config{};
 };
 
 } // namespace chessengine
