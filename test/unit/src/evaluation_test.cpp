1e1b91ff95092ec21d07892c552644a98a280050
Florian Giesemann <fgiesemann@proton.me>
2025-10-24 18:13:38 +0200

    Test piece value and material score evaluation

--- test/unit/src/evaluation_test.cpp
+++ test/unit/src/evaluation_test.cpp
@@ -23,6 +23,22 @@ TEST_CASE("Evaluation.Black mated", "[evaluation]") {
     CHECK(evaluator.evaluate(position, Color::Black) == -Score::Mate);
 }
 
-TEST_CASE("Evaluation.Material", "[evaluation]") {
-    FAIL();
+TEST_CASE("Evaluation.Material.Piece Values", "[evaluation]") {
+    EvaluatorConfig config{.piece_values = {Score{100}, Score{500}, Score{300}, Score{300}, Score{900}, Score{0}}};
+
+    CHECK(config.piece_value(PieceType::Pawn) == Score{100});
+    CHECK(config.piece_value(PieceType::Rook) == Score{500});
+    CHECK(config.piece_value(PieceType::Knight) == Score{300});
+    CHECK(config.piece_value(PieceType::Bishop) == Score{300});
+    CHECK(config.piece_value(PieceType::Queen) == Score{900});
+    CHECK(config.piece_value(PieceType::King) == Score{0});
+}
+
+TEST_CASE("Evaluation.Material.Position", "[evaluation]") {
+    Position position{FenString{"1k2q3/3r1pn1/2b4p/4n3/1P6/2N1B1PB/P7/2Q3KR w - - 0 1"}};
+    EvaluatorConfig config{.piece_values = {Score{100}, Score{500}, Score{300}, Score{300}, Score{900}, Score{0}}};
+    Evaluator evaluator{config};
+
+    CHECK(evaluator.countup_material(position, Color::White) == Score{2600});
+    CHECK(evaluator.countup_material(position, Color::Black) == Score{2500});
 }
