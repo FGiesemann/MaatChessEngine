0a45e18970179ce39abc0ce3740c66badfc159fe
Florian Giesemann <fgiesemann@proton.me>
2025-11-08 11:30:21 +0100

    Add all uci callbacks
    Mostly empty for now

--- src/app/engine.cpp
+++ src/app/engine.cpp
@@ -7,8 +7,29 @@
 
 namespace chessengine::maat {
 
+auto Engine::new_game() -> void {
+    m_position = chesscore::Position{};
+}
+
+auto Engine::start_search() -> void {
+    // TODO
+}
+
+auto Engine::stop_search() -> void {
+    // stop search thread
+}
+
+auto Engine::best_move() const -> chesscore::Move {
+    // TODO
+    return {};
+}
+
 auto Engine::set_position(const chesscore::Position &position) -> void {
     m_position = position;
+}
+
+auto Engine::set_debugging(bool on) -> void {
+    m_debugging = on;
 }
 
 } // namespace chessengine::maat
--- src/app/engine.h
+++ src/app/engine.h
@@ -12,10 +12,17 @@ namespace chessengine::maat {
 
 class Engine {
 public:
+    auto new_game() -> void;
     auto set_position(const chesscore::Position &position) -> void;
     auto position() const -> const chesscore::Position & { return m_position; }
+    auto set_debugging(bool on) -> void;
+
+    auto start_search(/* some options */) -> void;
+    auto stop_search() -> void;
+    auto best_move() const -> chesscore::Move;
 private:
     chesscore::Position m_position;
+    bool m_debugging{false};
 };
 
 } // namespace chessengine::maat
