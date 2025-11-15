f431783618c2cbb56d45b1d71e57fd427d6612aa
Florian Giesemann <florian.giesemann@gmail.com>
2025-11-15 17:09:09 +0100

    Add stopping criteria to search

--- src/chessengine/types.cpp
+++ src/chessengine/types.cpp
@@ -5,6 +5,8 @@
 
 #include "chessengine/types.h"
 
+#include <limits>
+
 namespace chessengine {
 
 const Score Score::Infinity{32700};
@@ -14,5 +16,6 @@ const Score Score::Mate{32000};
 const Depth Depth::Zero{0};
 const Depth Depth::Step{1};
 const Depth Depth::MaxMateDepth{256};
+const Depth Depth::Infinite{std::numeric_limits<Depth::value_type>::max()};
 
 } // namespace chessengine
