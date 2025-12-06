8ad0b19749af13fd43a0c77311e771000b394200
Florian Giesemann <fgiesemann@proton.me>
2025-12-06 14:01:08 +0100

    Make construct_position a free function

--- src/chessengine/uci_engine.cpp
+++ src/chessengine/uci_engine.cpp
@@ -4,3 +4,23 @@
  * ************************************************************************** */
 
 #include "chessengine/uci_engine.h"
+
+namespace chessengine {
+
+auto construct_position(const chessuci::position_command &command) -> std::pair<chesscore::Position, UCIMoveList> {
+    UCIMoveList move_list{};
+    const auto fen = command.fen == chessuci::position_command::startpos ? chesscore::FenString::starting_position() : chesscore::FenString{command.fen};
+    auto position = chesscore::Position{fen};
+    std::ranges::for_each(command.moves, [&position, &move_list](const chessuci::UCIMove &move) -> void {
+        const auto matched_move = chessuci::convert_legal_move(move, position);
+        if (matched_move.has_value()) {
+            position.make_move(matched_move.value());
+            move_list.push_back(move);
+        } else {
+            throw chessuci::UCIError{"Invalid move " + to_string(move)};
+        }
+    });
+    return std::make_pair(position, move_list);
+}
+
+} // namespace chessengine
