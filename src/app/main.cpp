82af57b8c1fab0038ed1db1b6a22ebbe615b649d
Florian Giesemann <fgiesemann@proton.me>
2025-12-08 18:35:11 +0100

    Enable logging with command line option

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -3,18 +3,23 @@
  * Chess playing engine                                                       *
  * ************************************************************************** */
 
+#include "chessengine/chess_engine.h"
+#include "chessengine/logger.h"
+#include "chessengine/uci_adapter.h"
+
 #include <iostream>
 #include <thread>
 
-#include "chessengine/chess_engine.h"
-#include "chessengine/uci_adapter.h"
-
-auto main() -> int {
+auto main(int argc, char *argv[]) -> int {
     chessengine::UCIAdapter<chessengine::ChessEngine> uci_adapter{std::cin, std::cout};
 
     auto config = uci_adapter.engine().config();
     config.search_config.iterative_deepening = true;
     uci_adapter.engine().set_config(config);
+
+    if (argc > 1 && std::string(argv[1]) == "--debug") {
+        chessengine::Logger::instance().enable("engine_debug.log");
+    }
 
     uci_adapter.run();
     return 0;
