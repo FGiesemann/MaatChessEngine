9f41ab8b07b0440baf6ecd572c600f08603e6a52
Florian Giesemann <fgiesemann@proton.me>
2026-03-08 15:56:39 +0100

    Fix email address

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,7 @@ class ChessEngineConan(ConanFile):
     package_type = "library"
 
     license = "MIT License"
-    author = "Florian Giesemann <florian.giesemann@gmail.com>"
+    author = "Florian Giesemann <fgiesemann@proton.me>"
     url = "https://github.com/FGiesemann/ChessEngine.git"
     homepage = "https://github.com/FGiesemann/ChessEngine"
 
